c90aef519eeeddcc72a40b48acbe212d2b56c6b8
Justin Mason <git@jmason.org>
2015-01-08 23:11:09 +0000

    Add null checking by default

---
+++ src/main/java/com/swrve/logging/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@DefaultAnnotation(Nonnull.class)
+package com.swrve.logging;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
