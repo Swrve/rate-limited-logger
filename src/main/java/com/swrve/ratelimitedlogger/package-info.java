b1e82972082cbe2c8fdf88ea4f2e78a288776525
Justin Mason <justin.mason@swrve.com>
2015-02-05 15:59:44 +0000

    Work around silly javadoc bug

--- src/main/java/com/swrve/ratelimitedlogger/package-info.java
+++ src/main/java/com/swrve/ratelimitedlogger/package-info.java
@@ -1,6 +1,7 @@
 /**
- * an SLF4J-compatible, simple, fluent API for rate-limited logging in Java.
+ * an SLF4J-compatible, simple, fluent API for rate-limited logging in Java; start with {@link com.swrve.ratelimitedlogger.RateLimitedLog}.
  */
+
 @ParametersAreNonnullByDefault
 @DefaultAnnotation(Nonnull.class)
 package com.swrve.ratelimitedlogger;
